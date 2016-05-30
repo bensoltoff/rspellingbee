61c34ff18e70310916ce9b71d0e7b3dc37227104
bensoltoff <ben@bensoltoff.com>
2016-05-30 19:16:14 -0400

    Clean up speller bios and impute gender and ethnicity

--- R/get_bios.R
+++ R/get_bios.R
@@ -75,3 +75,74 @@ get_bios <- function(seasons){
   return(results)
 }
 
+#' Clean spellers' biographies
+#'
+#' @param spellers
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clean_bios <- function(spellers){
+  # clean up existing attributes
+  spellers %<>%
+    na.omit %>%
+    # convert grade to numeric
+    dplyr::mutate(grade = stringr::word(grade, 1),
+           grade_num = ifelse(grade == "First", 1, NA),
+           grade_num = ifelse(grade == "Second", 2, grade_num),
+           grade_num = ifelse(grade == "Third", 3, grade_num),
+           grade_num = ifelse(grade == "Fourth", 4, grade_num),
+           grade_num = ifelse(grade == "Fifth", 5, grade_num),
+           grade_num = ifelse(grade == "Sixth", 6, grade_num),
+           grade_num = ifelse(grade == "Seventh", 7, grade_num),
+           grade_num = ifelse(grade == "Eighth", 8, grade_num),
+           grade = grade_num) %>%
+    dplyr::select(-grade_num) %>%
+    # extract state name
+    dplyr::mutate(state = stringr::str_extract(sponsor, paste0(state.name, collapse = "|"))) %>%
+    # home school
+    dplyr::mutate(homeschool = grepl("Home", school, ignore.case = TRUE)) %>%
+    # extract first name
+    dplyr::mutate(first_name = stringr::word(speller, 1))
+
+  # get gender
+  spellers_gender <- gender::gender(spellers$first_name) %>%
+    dplyr::select(name, gender) %>%
+    dplyr::rename(first_name = name)
+
+  spellers <- dplyr::left_join(spellers, spellers_gender) %>%
+    unique
+
+  # estimate ethnicity
+  ethnicity <- dplyr::data_frame(surname = stringr::word(spellers$speller, -1)) %>%
+    unique %>%
+    # calculate probabilities
+    wru::race.pred(surname.only = TRUE) %>%
+    tbl_df %>%
+    # gather predictions to tidy data
+    tidyr::gather(ethnicity, prob, pred.whi:pred.oth) %>%
+    # rename ethnic categories
+    dplyr::mutate(ethnicity = gsub("pred.", "", ethnicity),
+           ethnicity = ifelse(ethnicity == "whi", "white", ethnicity),
+           ethnicity = ifelse(ethnicity == "bla", "black", ethnicity),
+           ethnicity = ifelse(ethnicity == "his", "hispanic", ethnicity),
+           ethnicity = ifelse(ethnicity == "asi", "asian", ethnicity),
+           ethnicity = ifelse(ethnicity == "oth", "other", ethnicity)) %>%
+    # keep only most probable ethnicity
+    dplyr::group_by(surname) %>%
+    dplyr::filter(prob == max(prob))
+
+  spellers %<>%
+    dplyr::mutate(surname = stringr::word(speller, -1)) %>%
+    dplyr::left_join(ethnicity)
+
+  return(spellers)
+}
+
+
+
+
+
+
+
