8f93fbdbdafdc57cf2fd12e3575b4b1421a6b0d5
bensoltoff <ben@bensoltoff.com>
2016-05-30 16:52:04 -0400

    Add functions to get results

---
+++ R/get_results.R
@@ -0,0 +1,78 @@
+#' Get round results
+#'
+#' Get all results from a single round in the spelling bee
+#'
+#' @param round Round number
+#' @param season Season (e.g. 2012)
+#'
+#' @return
+#' @export
+#'
+#' @importFrom dplyr tbl_df
+#' @importFrom magrittr "%>%"
+#' @importFrom magrittr "%<>%"
+#'
+get_round_results <- function(round, season){
+  # get table from page
+  url <- paste0("http://spellingbee.com/public/results/", season, "/round_results/summary/", round)
+
+  # if no table returns, then no data available - exit
+  data <- tryCatch(xml2::read_html(url) %>%
+                     rvest::html_node("table"),
+                   error = function(e) NULL)
+  if(is.null(data)) return(NULL)
+
+  # convert to table
+  results <- data %>%
+    rvest::html_table() %>%
+    tbl_df
+
+  # fix column names
+  results %<>%
+    dplyr::rename(id = `No.`,
+           speller = `Speller's Name`,
+           sponsor = `Speller's Sponsor`,
+           word_correct = `Correct Spelling`,
+           word_given = `Spelling Given`,
+           error = Error) %>%
+    dplyr::mutate(error = error == "E",
+           id_round = row_number())
+
+  # add round and year info
+  results %<>%
+    dplyr::mutate(season = season, round = round)
+
+  return(results)
+}
+
+#' Get season rounds
+#'
+#' Get all rounds in a single competition
+#'
+#' @param season Season (e.g. 2012)
+#'
+#' @return
+#' @export
+#'
+get_season_rounds <- function(season){
+  rounds <- 2:40
+  results <- lapply(rounds, function(x) get_round_results(season = season, round = x)) %>%
+    dplyr::bind_rows(.)
+
+  return(results)
+}
+
+#' Get seasons
+#'
+#' Get round results from multiple seasons
+#'
+#' @param seasons Seasons in a vector
+#'
+#' @return
+#' @export
+#'
+get_seasons <- function(seasons){
+  results <- lapply(seasons, get_season_rounds) %>%
+    dplyr::bind_rows(.)
+  return(results)
+}
